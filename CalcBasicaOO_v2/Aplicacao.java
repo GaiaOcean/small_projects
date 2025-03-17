24751300df44647bec59aa027d8f11f77d33ad88
Luana Macedo <92169736+luanagbrm@users.noreply.github.com>
2025-03-16 23:12:42 -0300

    Implementa métodos de verificação de entrada e entrada por meio de janela gráfica
    - Adicionados métodos de verificação em tempo de input
    - Adicionada a opção de entrada por janela gráfica se enviado "g" nos parametros de chamada do main
    - Implementado o conceito de "least privilege access" em todos os métodos por meio de encapsulamento
    - Excluida a classe "EntradaGrafica"

--- CalcBasicaOO_v2/Aplicacao.java
+++ CalcBasicaOO_v2/Aplicacao.java
@@ -1,10 +1,11 @@
-
 /**
  * Classe principal.
  * 
- * @author Julio Arakaki 
- * @version 20250310
+ * @author 
+ * @version 
  */
+
+import javax.swing.JOptionPane;
 public class Aplicacao {
 
     /**
@@ -13,42 +14,31 @@ public class Aplicacao {
      * @param args - vetor de string (recebe dados do SO)
      */
     public static void main(String args[]){
-
-        // Verifica se existe parametros vindos do SO (args)
-        if (args.length == 0){ // sem parametros (faz entrada pelo teclado)
-
-            // Instancia objeto para entrada de string (cria um objeto da classe)
+        boolean useGUI = false;
+        
+        if(args.length == 1){
+                // Instancia objeto para entrada de string (cria um objeto da classe)
             Entrada ent = new Entrada();
-            
-            String choice = ent.lerString("Digite 0 para acessar a calculadora Básica e 1 para a calculadora científica: ");
-            
+            ent.definirModo(args[0]);
+                    
+            String choice = ent.lerDados("Digite 0 para acessar a calculadora Básica \nou \n1 para a calculadora científica: ");
+                
             if(choice.equals("0")){
-                     // Le os dados do teclado
-                String operando1 = ent.lerString("Forneca operando1: ");
-                String operador = ent.lerString("Forneca operador: ");
-                String operando2 = ent.lerString("Forneca operando2: ");
-    
-                // Chamada do metodo local (calcular)
-                calcular(operando1, operador, operando2);
-            }else if(choice.equals("1")){
-                String operando = ent.lerString("Forneca operando: ");
-                CalcCientifica.calcularOperacao(operando);
-            }   
-        } else if (args.length == 3) { // parametros pelo SO (args)
-            // Le os dados da linha de comando
-            String operando1 = args[0];
-            String operador = args[1];
-            String operando2 = args[2];
-
-            // Chamada do metodo local (calcular)
-            calcular(operando1, operador, operando2);
-
-        }else if(args.length == 1){
-            String operando = args[0];
-            CalcCientifica.calcularOperacao(operando);
-        }else {
-            //System.out.println("Parametros invalidos!!");
-            Saida.mostrarMsg("Parametros invalidos!!");
+                // Le os dados do teclado
+                String operando1 = ent.lerOperacaoCalcBasica(1);
+                String operador = ent.lerOperacaoCalcBasica(2);
+                String operando2 = ent.lerOperacaoCalcBasica(3);
+        
+                // Chamada do metodo local (calcularBasica)
+                calcularBasica(operando1, operador, operando2, ent.getModoGrafico());
+            } else if (choice.equals("1")){
+                String operador = ent.lerOperacaoCalcCientifica(2);
+                String operando = ent.lerOperacaoCalcCientifica(1);
+                
+                calcularCientifica(operando,operador,ent.getModoGrafico());
+            }
+        } else {
+            Saida.mostrarMsg("Parametros invalidos. Digite \"t\" para modo texto ou \"g\" para modo grafico", false);
         }
     }
 
@@ -59,12 +49,20 @@ public class Aplicacao {
      * @param s2 - operador
      * @param s3 - operando2
      */
-    public static void calcular(String s1, String s2, String s3){
+    private static void calcularBasica(String s1, String s2, String s3, boolean useGUI){
         // Cria instancia (criando um obj) da classe CalcBasica
         CalcBasica cb = new CalcBasica();
 
         // Aciona o metodo calcular (passa o vetor de strings, como parametro)
-        cb.calcular(s1, s2, s3);
+        cb.calcular(s1, s2, s3, useGUI);
+    }
+    
+    private static void calcularCientifica(String s1, String s2, boolean useGUI){
+        // Cria instancia (criando um obj) da classe CalcBasica
+        CalcCientifica cb = new CalcCientifica();
+
+        // Aciona o metodo calcular (passa o vetor de strings, como parametro)
+        cb.calcular(s1, s2,useGUI);
     }
 }
 
--- CalcBasicaOO_v2/CalcCientifica.java
+++ CalcBasicaOO_v2/CalcCientifica.java
@@ -1,53 +1,40 @@
-
-/**
- * Escreva uma descrição da classe CalcCientifica aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
- */
 public class CalcCientifica
 {
-    public static void calcularOperacao( String operando){
+    public void calcular(String operando, String operador, boolean modoGrafico){
         
         double result = 0;
         String msg = "";
-        CalcBasica calc = new CalcBasica();
         
-        String operador = chooseOperation();
-        if(calc.isNumeric(operando)){
-            switch(Integer.parseInt(operador)){
-                case 1:
-                    result = Math.sin(Double.parseDouble(operando));
-                    msg = "Seno de " + operando + " = " + result;
-                    break;
-                case 2:
-                    result = fatorial(operando);
-                    if (result != -1) {  // Verifica se o fatorial foi válido
-                        msg = "Fatorial de " + operando + " = " + result;
-                    } else {
-                        msg = "Fatorial não pode ser calculado para números negativos";
-                    }
-                    break;
-                case 3:
-                    result = Math.sqrt(Double.parseDouble(operando));
-                    msg = "Raiz quadrada  de " + operando + " = " + result;
-                    break;
-                default:
-                    msg = "Operador Inválido";
+        switch(Integer.parseInt(operador)){
+            case 1:
+                result = Math.sin(Double.parseDouble(operando));
+                msg = "Seno de " + operando + " = " + result;
+                break;
+            case 2:
+                result = fatorial(operando, modoGrafico);
+                if (result != -1) {  // Verifica se o fatorial foi válido
+                    msg = "Fatorial de " + operando + " = " + result;
+                } else {
+                    msg = "Fatorial não pode ser calculado para números negativos";
+                }
+                break;
+            case 3:
+                result = Math.sqrt(Double.parseDouble(operando));
+                msg = "Raiz quadrada  de " + operando + " = " + result;
+                break;
+            default:
+                msg = "Operador Inválido";
             }
-        }else{
-            msg = "Operando inválido";
-        }
         
-        Saida.mostrarMsg(msg);
+        Saida.mostrarMsg(msg,modoGrafico);
     }
     
-    public static double fatorial(String operando){
+    private static double fatorial(String operando, boolean modoGrafico){
         double result = 1;
         int n = Integer.parseInt(operando);
         
         if(n < 0){
-            Saida.mostrarMsg("Operando inválido");
+            Saida.mostrarMsg("Operando inválido", modoGrafico);
             return -1;
         }
         for(int i = n; i > 0; i--){
@@ -55,14 +42,5 @@ public class CalcCientifica
         }
         
         return result; 
-    }
-    
-    public static String chooseOperation(){
-        String msg = "Digite \n 1 - seno \n 2 - Fatorial \n 3 - Raiz Quadrada";
-        Saida.mostrarMsg(msg);
-        
-        String choice = Entrada.retornaString();
-        
-        return choice;
     }
 }
